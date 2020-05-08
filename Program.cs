1a2daba3c681cbf06b2f9054a1e1ac565fc53189
Maria Luiza <malualper@hotmail.com>
2020-05-07 22:09:48 -0300

    contagemregressiva

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace contagemregressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("preparar!");
+            Console.ReadLine();
+            Console.WriteLine("apontar!");
+            Console.ReadLine();
+            Console.WriteLine("fogo!");
+            Console.ReadLine();
+        }
+    }
+}
